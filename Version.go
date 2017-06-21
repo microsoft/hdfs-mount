2ec3dfcbb9400e7ba8419ec6c2788e7c35679cfc
Junjie Qian <juqia@microsoft.com>
2017-06-21 13:54:43 -0700

    Add version and built info
    Use current GIT commit ID and built info (time and machine) for versioning purpose

---
+++ Version.go
@@ -0,0 +1,15 @@
+package main
+
+var (
+	//TODO: Add Version tag, manually update
+	// VERSION = "0.0.1"
+
+	// GITCommit overwritten automatically by the build
+	GITCOMMIT = "HEAD"
+
+	// Built time overwritten automatically by the build
+	BUILDTIME = "NOW"
+
+	// Built hostname overwritten automatically by build
+	HOSTNAME = "LOCALHOST"
+)
